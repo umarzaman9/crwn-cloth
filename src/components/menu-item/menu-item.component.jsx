01ebcabc108c7b028319e8b9569c70f22a287d6f
umarzaman9 <umarofficial9@gmail.com>
2021-07-20 14:10:56 +0500

    adding Navigation Routing

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => {
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => {
 	 return (
-	 	<div className={`${size} menu-item`}>
+	 	<div 
+	 	className={`${size} menu-item`} 
+	 	onClick = {() => history.push(`${match.url}${linkUrl}`)} 
+	 	>
 		 	<div 
-		 	className='background-image' 
-		 	style= {{backgroundImage: `url(${title, imageUrl})` }} 
+		 	className='background-image'  // eslint-disable-next-line 
+		 	style= {{backgroundImage: `url(${title , imageUrl})` }} 
 		 	/>
 			<div className='content'>
 				<h1 className='title'> {title.toUpperCase()} </h1>
@@ -16,4 +21,4 @@ const MenuItem = ({title, imageUrl, size}) => {
 	 );
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
