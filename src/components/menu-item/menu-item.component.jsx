7061d0288e94e9dc23d73399f652b1ab0406d14e
umarzaman9 <umarofficial9@gmail.com>
2021-07-27 16:34:32 +0500

    adding firebase

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => {
-	 return (
-	 	<div 
-	 	className={`${size} menu-item`} 
-	 	onClick = {() => history.push(`${match.url}${linkUrl}`)} 
-	 	>
-		 	<div 
-		 	className='background-image'  // eslint-disable-next-line 
-		 	style= {{backgroundImage: `url(${title , imageUrl})` }} 
-		 	/>
-			<div className='content'>
-				<h1 className='title'> {title.toUpperCase()} </h1>
-				<span className='subtitle'> SHOP NOW </span> 
-			</div>		
-		</div>
-	 );
-}
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+);
 
 export default withRouter(MenuItem);
