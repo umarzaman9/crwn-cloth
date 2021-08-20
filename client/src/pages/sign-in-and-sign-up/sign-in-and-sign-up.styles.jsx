54d17718ef371bc23b05e888d5025f95d5120d0f
umarzaman9 <umarofficial9@gmail.com>
2021-08-20 13:53:40 +0500

    adjusting mobile view

--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
@@ -9,6 +9,7 @@ export const SignInAndSignUpContainer = styled.div`
   @media screen and (max-width: 800px) {
     flex-direction: column;
     width: unset;
+	margin: 30px;
     align-items: center;
     > *:first-child {
       margin-bottom: 50px;
